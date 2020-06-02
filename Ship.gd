5ff18cfbb0b7c7967216ae1f8c19bd98c0fc6bb7
Drew Polstra <dibujaron@gmail.com>
2020-06-01 21:51:56 -0400

    Modify docking to not use godot's child system, eliminating flicker

--- Ship.gd
+++ Ship.gd
@@ -24,7 +24,10 @@ var fore_thrusters_active = false
 var aft_thrusters_active = false
 var tiller_left = false
 var tiller_right = false
-var docked = false
+
+var docked_to_station = null
+var docked_to_port = null
+var docked_from_port = null
 var currently_inside_planet = false
 var auto_control_script = {} #integer indexed map of steps.
 var next_auto_step = 0 #next step to execute
@@ -44,6 +47,10 @@ export var default_secondary_color = Color.white
 func _ready():
 	_set_primary_color(default_primary_color)
 	_set_secondary_color(default_secondary_color)
+	
+func docked():
+	return docked_to_station and docked_from_port and docked_to_port
+	
 func _set_primary_color(color):
 	$Color1.modulate = color
 	$Color1Shaded.modulate = Color.from_hsv(color.h, color.s, color.v - 0.1)
@@ -69,37 +76,36 @@ func json_init(json):
 	initialized = true
 
 func json_receive_docked(json):
-	docked = true;
 	var my_port_info = json["ship_port"]
 	var station_name = json["station_identifying_name"]
 	var station_port = json["station_port"]
 	var stations = get_tree().get_nodes_in_group("Stations")
 	for station in stations:
 		if station.orbiter_name == station_name:
-			var space_node = get_tree().get_root().get_node("Space")
-			space_node.remove_child(self)
-			station.add_child(self)
-			velocity = Vector2(0,0)
-			var station_port_relative = json_to_vec(station_port["relative_position"])
-			var my_port_relative = json_to_vec(my_port_info["relative_position"])
-			rotation = station_port["relative_rotation"] + my_port_info["relative_rotation"]
-			print("got docked.")
-			rotation_error = 0.0
-			position = station_port_relative + (my_port_relative * -1.0).rotated(rotation)
-	pass
+			docked_to_station = station
+			break
+			#var space_node = get_tree().get_root().get_node("Space")
+			#space_node.remove_child(self)
+			#station.add_child(self)
+			#velocity = Vector2(0,0)
+			#var station_port_relative = json_to_vec(station_port["relative_position"])
+			#var my_port_relative = json_to_vec(my_port_info["relative_position"])
+			#rotation = station_port["relative_rotation"] + my_port_info["relative_rotation"]
+			#rotation_error = 0.0
+			#position = station_port_relative + (my_port_relative * -1.0).rotated(rotation)
+	docked_from_port = my_port_info
+	docked_to_port = station_port
 	
 func json_receive_undocked(json):
-	docked = false
 	var expected_rotation = json["rotation"]
-	var space_node = get_tree().get_root().get_node("Space")
-	var par = get_parent()
-	par.remove_child(self)
-	space_node.add_child(self)
 	global_rotation = expected_rotation
 	var expected_position = json_to_vec(json["global_pos"])
 	global_position = expected_position
 	var expected_velocity = json_to_vec(json["velocity"])
 	velocity = expected_velocity
+	docked_to_station = null
+	docked_from_port = null
+	docked_to_port = null
 	
 var left_press_time = 0
 var left_press_start_tick = 0
@@ -110,16 +116,8 @@ func json_update_inputs(json):
 	starboard_thrusters_active = json["stbd_thrusters"]
 	fore_thrusters_active = json["fore_thrusters"]
 	aft_thrusters_active = json["aft_thrusters"]
-	var server_left = json["rotating_left"]
-	#if server_left and not tiller_left:
-	#	left_press_time = OS.get_ticks_msec()
-	#	left_press_start_tick = tick_count
-	#	left_press_start_phys_tick = physics_tick_count 
-	#elif not server_left and tiller_left:
-	#	print("left pressed for ", (OS.get_ticks_msec() - left_press_time), "ms, ", (tick_count - left_press_start_tick), " main ticks, ", (physics_tick_count - left_press_start_phys_tick), " phys ticks")
-	tiller_left = server_left
-	var server_right = json["rotating_right"]
-	tiller_right = server_right
+	tiller_left = json["rotating_left"]
+	tiller_right = json["rotating_right"]
 	for engine in main_engines:
 		engine.set_enabled(main_engines_active)
 	for thruster in port_thrusters:
@@ -135,7 +133,7 @@ func json_update_inputs(json):
 var last_sync_time = 0.0
 func json_sync_state(json):
 	var sync_delta = (OS.get_ticks_msec() - last_sync_time) / 1000.0
-	if not docked:
+	if not docked():
 		if manually_controlled(): #this shouldn't only be in manual mode
 			var expected_rotation = json["rotation"]
 			global_rotation = expected_rotation
@@ -172,7 +170,14 @@ var tick_count = 0
 var physics_tick_count = 0
 func _process(delta):
 	#only things where the tick rate doesn't matter
-	if not docked:
+	if docked():
+		var my_port_relative = json_to_vec(docked_from_port.relative_position)
+		var station_port_relative = json_to_vec(docked_to_port.relative_position)
+		var rotation_offset = docked_to_port.relative_rotation + docked_from_port.relative_rotation
+		global_rotation = docked_to_station.global_rotation + rotation_offset
+		var docked_to_global_pos = docked_to_station.global_position + station_port_relative.rotated(docked_to_station.global_rotation)
+		global_position = docked_to_global_pos + (my_port_relative * -1.0).rotated(global_rotation)
+	else:
 		if manually_controlled():
 			if main_engines_active:
 				velocity += Vector2(0,-main_engine_thrust).rotated(global_rotation) * delta
@@ -203,7 +208,7 @@ func _physics_process(delta):
 	if auto_control_script.empty(): # manually controlled
 		velocity += get_gravity_acceleration() * delta
 	else:
-		if docked:
+		if docked():
 			next_auto_step = 0
 			auto_control_script.clear()
 		else:
