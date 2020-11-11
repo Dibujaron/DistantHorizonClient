14212f19bab7593ef2b7d3e35ed12d9a9777d6a3
Drew Polstra <dibujaron@gmail.com>
2020-11-10 23:27:40 -0500

    Breadcrumbs v4

--- scenes/player/Breadcrumb.gd
+++ scenes/player/Breadcrumb.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 export var breadcrumb_interval_base = 1.0
 export var calculation_steps_per_base_interval = 10.0
-export var projections_to_keep = 20
-var step_length = breadcrumb_interval_base / calculation_steps_per_base_interval
+export var projections_to_keep = 20.0
+var step_length = 1.0/60.0#breadcrumb_interval_base / calculation_steps_per_base_interval
 var my_ship = null
 var recent_projections = []
 var target_position = Vector2(0,0)
@@ -28,18 +28,22 @@ func _process(delta):
 		var ship_rotation = my_ship.global_rotation
 		var main_engine_thrust = my_ship.main_engine_thrust
 		var manu_engine_thrust = my_ship.manu_engine_thrust
-		velocity_projected += Global.get_gravity_acceleration(position_projected) * delta
-		position_projected += velocity_projected * delta
+		velocity_projected += Global.get_gravity_acceleration(position_projected) * step_length
+		position_projected += velocity_projected * step_length
 		
-	recent_projections.push_front(position_projected)
+	recent_projections.push_front([position_projected, velocity_projected])
 	if recent_projections.size() > projections_to_keep:
 		recent_projections.pop_back()
 	
-	var sum = Vector2(0,0)
+	var sum_position = Vector2(0,0)
+	var sum_velocity = Vector2(0,0)
 	for proj in recent_projections:
-		sum += proj
-	var avg = sum / recent_projections.size()
-	global_position = avg
+		sum_position += proj[0]
+		sum_velocity += proj[1]
+	var avg_position = sum_position / recent_projections.size()
+	var avg_velocity = sum_velocity / recent_projections.size()
+	global_rotation = avg_velocity.angle()
+	global_position = avg_position
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
