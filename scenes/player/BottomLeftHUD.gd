7aae766998d3050d87175b0ef7269d8911dee846
Drew Polstra <dibujaron@gmail.com>
2020-12-26 13:26:42 -0500

    Fix distance linking

--- scenes/player/BottomLeftHUD.gd
+++ scenes/player/BottomLeftHUD.gd
@@ -20,13 +20,7 @@ func _process(delta):
 	pass
 		
 func link_ship(ship):
-	if linked_ship:
-		print("error attempted to link ship to HUD but already linked.")
-		print("existing ship: ", linked_ship)
-		print("new ship: ", ship)
-	else:
-		print("linked ship to hud.")
-		linked_ship = ship
+	linked_ship = ship
 		
 func get_linked_ship():
 	return linked_ship
