658571d691871437aed1d94ae9df61702f67ca51
dibujaron <dibujaron@gmail.com>
2021-12-28 23:20:14 -0500

    Better breadcrumbs

--- scenes/player/Breadcrumb.gd
+++ scenes/player/Breadcrumb.gd
@@ -1,38 +1,23 @@
 extends Node2D
 
-export var calculation_steps = 6
-export var projections_to_keep = 5
-var step_length = 1.0/10.0#1.0/60.0
+export var projections_to_keep = 10
 
 var recent_projections_pos = []
 var recent_projections_vel = []
-var target_position = Vector2(0,0)
-
-var best_velocity = Vector2(0,0)
-var best_position = Vector2(0,0)
-var max_future_ticks = 0
+var projected_position = Vector2(0,0)
+var projected_velocity = Vector2(0,0)
 func _process(_delta):
 	var zoom = Global.get_current_zoom()
-
 	global_scale.x = zoom
 	global_scale.y = zoom
-	var parent = get_parent()
-	max_future_ticks = parent.max_future_ticks + (calculation_steps * step_length * 60)
-	var position_projected = parent.best_position
-	var velocity_projected = parent.best_velocity
-	for i in range(0, calculation_steps):
-		var future_distance_ticks = parent.max_future_ticks + (step_length * 60 * i)
-		position_projected += velocity_projected * step_length
-		var acceleration = Global.get_gravity_acceleration_at_tick(position_projected, future_distance_ticks) * step_length
-		velocity_projected += acceleration
-	recent_projections_pos.push_front(position_projected)
+	recent_projections_pos.push_front(projected_position)
 	if recent_projections_pos.size() > projections_to_keep:
 		recent_projections_pos.pop_back()
 	
-	recent_projections_vel.push_front(velocity_projected)
+	recent_projections_vel.push_front(projected_velocity)
 	if recent_projections_vel.size() > projections_to_keep:
 		recent_projections_vel.pop_back()
-	
+		
 	var sum_position = Vector2(0,0)
 	var sum_velocity = Vector2(0,0)
 	for proj_pos in recent_projections_pos:
@@ -43,7 +28,5 @@ func _process(_delta):
 		
 	var avg_position = sum_position / recent_projections_pos.size()
 	var avg_velocity = sum_velocity / recent_projections_vel.size()
-	best_velocity = velocity_projected
-	best_position = position_projected
-	global_rotation = avg_velocity.angle()
 	global_position = avg_position
+	global_rotation = avg_velocity.angle()
--- scenes/player/Breadcrumbs.gd
+++ scenes/player/Breadcrumbs.gd
@@ -1,20 +1,32 @@
 extends Node2D
 
 export var num_breadcrumbs = 5
-var best_velocity = Vector2(0,0)
-var best_position = Vector2(0,0)
-var max_future_ticks = 0
-#todo use relative position to avoid jitter
+
+export var calculation_step_length_seconds = 1.0/10.0
+export var calculation_steps_per_breadcrumb = 6
+var breadcrumbs = []
 func _ready():
 	var breadcrumb_scene = preload("res://scenes/player/Breadcrumb.tscn")
-	var last_parent = self
 	for _i in range(0, num_breadcrumbs):
 		var breadcrumb = breadcrumb_scene.instance()
-		last_parent.add_child(breadcrumb)
-		last_parent = breadcrumb
+		add_child(breadcrumb)
+		breadcrumbs.append(breadcrumb)
 		
 func _process(_delta):
 	var ship = get_parent()
 	visible = not ship.docked()
-	best_velocity = ship.velocity
-	best_position = ship.global_position
+	
+	var seconds_in_future = 0.0
+	var projected_position = ship.global_position
+	var projected_velocity = ship.velocity
+	
+	for breadcrumb_index in range(0, num_breadcrumbs):
+		var breadcrumb = breadcrumbs[breadcrumb_index]
+		for calculation_step_index in range(0, calculation_steps_per_breadcrumb):
+			seconds_in_future += calculation_step_length_seconds
+			projected_position += (projected_velocity * calculation_step_length_seconds)
+			var ticks_in_future = seconds_in_future / 60.0
+			var acceleration = Global.get_gravity_acceleration_at_tick(projected_position, ticks_in_future) * calculation_step_length_seconds
+			projected_velocity += acceleration
+		breadcrumb.projected_position = Vector2(projected_position.x, projected_position.y)
+		breadcrumb.projected_velocity = Vector2(projected_velocity.x, projected_velocity.y)
